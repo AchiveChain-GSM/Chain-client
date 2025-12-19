4d8db57def962579bc96757490ae4c83f69298b0
uifnswi <tnwlspearl@gmail.com>
2025-12-19 23:39:46 +0900

    Revert "feat: 타임라인 페이지 기본 구조 구현"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Timeline from './pages/Timeline';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/timeline" element={<Timeline />} />
+       
       </Routes>
     </BrowserRouter>
   );
