7bc2424c39b3bb377d86c46b3ce330b7b43d9988
jxomi <jxomi80@gmail.com>
2025-12-22 11:00:20 +0900

    feat: 회원가입 단계별 로직 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Login from "./Login";
-import Timeline from "./pages/Timeline.jsx";
+import topbar from './components/topbar';
+import Login from './Login';
+import Timeline from './pages/Timeline.jsx';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div style={{ backgroundColor: "#191919", minHeight: "100vh" }}>
+      <div style={{ backgroundColor: '#191919', minHeight: '100vh' }}>
+        <topbar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Timeline />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </BrowserRouter>
