79985b75ef8ce525ad0f8b68a98878f6b668e330
jxomi <jxomi80@gmail.com>
2025-12-23 15:40:07 +0900

    fix: 주소 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      // 프론트에서 /api로 시작하는 요청을 보내면 아래 target 주소로 전달합니다.
+      '/api': {
+        target:
+          'https://port-0-chain-server-mjgfqy3sbea3654a.sel3.cloudtype.app/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // 경로 유지
+      },
+    },
+  },
 });
